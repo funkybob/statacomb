6855df22b650a126050b886d105476a1cb69e0dc
funkybob <curtis@tinbrain.net>
2015-02-24 15:52:45 +1100

    Add basic setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name='statacomb',
+    version='0.0.1',
+    description='A simple stats gather/display app using PG JSON field.',
+    author='Curtis Maloney',
+    author_email='curtis@tinbrain.net',
+    url='http://github.com/funkybob/statacomb/',
+    packages=find_packages(),
+    package_data={
+        'statacomb.web': ['templates/*'],
+    },
+    zip_safe=False,
+    install_requires=[
+        'psycopg2>=2.6',
+        'antfarm==0.0.1',
+    ],
+)
